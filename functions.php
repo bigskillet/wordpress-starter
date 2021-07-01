7fece6ce8b49832b854eaf304ec45c86d21eef90
bigskillet <matt@bigskillet.com>
2021-07-01 16:34:07 -0500

    Add core-block-patterns

--- functions.php
+++ functions.php
@@ -40,9 +40,6 @@ add_action( 'after_setup_theme', function() {
 	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 	 */
   add_theme_support( 'post-thumbnails' );
-
-  // Gutenberg
-  remove_theme_support( 'core-block-patterns' );
 });
 
 /**
